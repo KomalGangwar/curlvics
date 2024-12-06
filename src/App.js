2e2e90bb0f1ab275bf7db4f011ba1d57917241ed
Komal  Gangwar <107317696+KomalGangwar@users.noreply.github.com>
2024-12-06 19:30:10 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./App.css";
 
 const App = () => {
+  const [message,setMessage]=useState("");
+  const [status,setStatus]=useState("")
   return (
-    <div>
-      
+    <div className='App'>
+      <h1>Send Mail</h1>
+      <textarea placeholder='Write your message here...' value={message} onChange={(e)=>setMessage(e.target.value)} rows={5}/>
+      <button onClick={SendMail}>Send Mail</button>
+      {status && <p>{status}</p>}
     </div>
   )
 }
 
 export default App
+
